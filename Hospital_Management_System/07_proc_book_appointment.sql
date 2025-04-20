476214ce2528259eac2c28fb1b95fa4deb79b64c
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-04-20 16:51:30 +0530

    Add files via upload

---
+++ Hospital_Management_System/07_proc_book_appointment.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE PROCEDURE book_appointment (
+	IN in_patient_id INT,
+    IN in_doctor_id INT,
+    IN in_appoitment_date DATE
+)
+BEGIN
+	INSERT INTO Appointments(patient_id, doctor_id, appointment_date, status)
+    VALUES(in_patient_id, in_doctor_id, in_appointment_date, 'Scheduled');
+END $$
+
+DELIMITER ;
