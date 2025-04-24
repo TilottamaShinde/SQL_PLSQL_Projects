f60516d8751cf74db031befac6093dfbb77a985e
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-04-24 21:24:21 +0530

    Update 11_proc_generate_bill.sql
    added a new table in schema named billing_log and updated generate_bill procedure to insert values in the billing log table with the current timestamp.

--- Hospital_Management_System/11_proc_generate_bill.sql
+++ Hospital_Management_System/11_proc_generate_bill.sql
@@ -1,3 +1,6 @@
+DROP PROCEDURE IF EXISTS generate_bill;
+
+
 DELIMITER $$ 
 
 CREATE PROCEDURE generate_bill (
@@ -6,11 +9,20 @@ CREATE PROCEDURE generate_bill (
 )
 
 BEGIN
+	DECLARE bill_date DATE;
+    SET bill_date = CURDATE();
+    
+    -- insert into billing table
 	INSERT INTO Billing(Appointment_id, amount, billing_date)
     VALUES (in_appointment_id, in_amount, CURDATE());
+    
+    -- insert into billing_log table
+    INSERT INTO billing_log (appointment_id, amount, billing_date)
+    VALUES (in_appointment_id, in_amount, bill_date);
+    
 END $$
 
 DELIMITER ;
 
 
--- CALL generate_bill (101, 2500.00);
+ -- CALL generate_bill (102, 2900.00);
