31a3de7b709f3867658ef01b184019a7b101652e
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-04-27 23:44:15 +0530

    Add files via upload

---
+++ Student_Performance_Tracker/02_Create_Tables.sql
@@ -0,0 +1,17 @@
+-- Script for creating tables
+
+CREATE TABLE Students (
+	student_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender ENUM('Male','Female','Other') NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    phone_number VARCHAR(15),
+    address TEXT,
+    enrollment_date DATE NOT NULL,
+    status ENUM('Active','Inactive') DEFAULT 'Active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    
+);
