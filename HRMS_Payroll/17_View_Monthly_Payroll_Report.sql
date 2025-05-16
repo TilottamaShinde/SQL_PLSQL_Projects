91618bc910f6fd06e0529fc80f0c2894dc22d514
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-16 23:13:52 +0530

    Update 17_View_Monthly_Payroll_Report.sql

--- HRMS_Payroll/17_View_Monthly_Payroll_Report.sql
+++ HRMS_Payroll/17_View_Monthly_Payroll_Report.sql
@@ -1,1 +1,26 @@
+-- 
 CREATE VIEW Monthly_Payroll_Report AS
+SELECT 
+	ep.payslip_id,
+    ep.emp_id,
+    CONCAT(e.first_name, ' ', e.last_name) AS Employee_name,
+    ep.pay_month,
+    ep.basic_salary,
+    ep.hra,
+    ep.special_allowances,
+    ep.other_allowances,
+    ep.total_earnings,
+    ep.total_deductions,
+    ep.net_pay,
+    ep.status,
+    ep.generated_on
+FROM
+	employee_payslip ep
+JOIN 
+	employee e
+    ON ep.emp_id = e.emp_id
+ORDER BY
+	ep.pay_month DESC, employee_name;
+
+-- Example Usage
+-- select * from Monthly_Payroll_Report;
