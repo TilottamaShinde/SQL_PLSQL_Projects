3fb2fcbba7d493981d585c14c57ad0c0e01752f5
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-24 00:06:02 +0530

    Update 23_Proc_generate_employee_payslip.sql

--- HRMS_Payroll/23_Proc_generate_employee_payslip.sql
+++ HRMS_Payroll/23_Proc_generate_employee_payslip.sql
@@ -1,3 +1,6 @@
+-- 
+DROP PROCEDURE generate_employee_payslip;
+
 DELIMITER //
 CREATE PROCEDURE generate_employee_payslip(
 	IN p_emp_id INT,
@@ -16,26 +19,34 @@ BEGIN
     DECLARE v_net_pay DECIMAL(10,2);
     
     -- Get latest salary details
-    SELECT basic_salary, hra, special_allowance, other_allowance, pf_deduction, professioanl_tax, income_tax
+    SELECT basic_salary, hra, special_allowance, other_allowance, pf_deduction, professional_tax, income_tax
     INTO v_basic, v_hra, v_special, v_other, v_pf, v_pt, v_it
     FROM employee_salary
     WHERE emp_id = p_emp_id
     ORDER BY effective_from DESC
     LIMIT 1;
     
-    SET v_total_earnings = v_basic + v_hra + v_special + v_other;
+ --   SET v_total_earnings = v_basic + v_hra + v_special + v_other;
     SET v_total_deductions = v_pf + v_pt + v_it;
-    SET v_net_pay = v_total_earnings - v_total_deductions;
-    
+ --   SET v_net_pay = v_total_earnings - v_total_deductions;
+    IF NOT EXISTS (
+		SELECT 1 FROM employee_payslip
+        WHERE emp_id = p_emp_id AND pay_month = p_pay_month
+        ) THEN 
     -- Insert payslip
-    INSERT INTO employee_payslip (
-		emp_id, pay_month, basic_salary, hra, special_allowances, other_allowance,
-        total_earnings, total_deductions, net_pay, status, generated_on, remarks
-    )
-    VALUES (
-		p_emp_id, p_pay_month, v_basic, v_hra, v_special, v_other,
-        v_total_earnings, v_total_deductions, v_net_pay, 'Finalized', NOW(), 'Auto-generated'
-    );
+		INSERT INTO employee_payslip (
+			emp_id, pay_month, basic_salary, hra, special_allowances, other_allowances, 
+			total_deductions, status, generated_on, remarks
+		)
+		VALUES (
+			p_emp_id, p_pay_month, v_basic, v_hra, v_special, v_other,
+			v_total_deductions,  'Finalized', NOW(), 'Auto-generated'
+		);
+    END IF;
    END //
    
    DELIMITER ;
+
+    
+-- EXAMPLE USAGE    
+--  CALL generate_employee_payslip(1, '2025-05-01');
