94a2eaac4533c8baf1ba64fb26431893e58beb3b
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-05-23 23:25:10 +0530

    Update 23_Proc_generate_employee_payslip.sql

--- HRMS_Payroll/23_Proc_generate_employee_payslip.sql
+++ HRMS_Payroll/23_Proc_generate_employee_payslip.sql
@@ -1,4 +1,41 @@
+DELIMITER //
 CREATE PROCEDURE generate_employee_payslip(
 	IN p_emp_id INT,
     IN p_pay_month DATE
 )
+BEGIN
+	DECLARE v_basic DECIMAL(10,2);
+    DECLARE v_hra DECIMAL(10,2);
+    DECLARE v_special DECIMAL(10,2);
+    DECLARE v_other DECIMAL(10,2);
+    DECLARE v_pf DECIMAL(10,2);
+    DECLARE v_pt DECIMAL(10,2);
+    DECLARE v_it DECIMAL(10,2);
+    DECLARE v_total_earnings DECIMAL(10,2);
+    DECLARE v_total_deductions DECIMAL(10,2);
+    DECLARE v_net_pay DECIMAL(10,2);
+    
+    -- Get latest salary details
+    SELECT basic_salary, hra, special_allowance, other_allowance, pf_deduction, professioanl_tax, income_tax
+    INTO v_basic, v_hra, v_special, v_other, v_pf, v_pt, v_it
+    FROM employee_salary
+    WHERE emp_id = p_emp_id
+    ORDER BY effective_from DESC
+    LIMIT 1;
+    
+    SET v_total_earnings = v_basic + v_hra + v_special + v_other;
+    SET v_total_deductions = v_pf + v_pt + v_it;
+    SET v_net_pay = v_total_earnings - v_total_deductions;
+    
+    -- Insert payslip
+    INSERT INTO employee_payslip (
+		emp_id, pay_month, basic_salary, hra, special_allowances, other_allowance,
+        total_earnings, total_deductions, net_pay, status, generated_on, remarks
+    )
+    VALUES (
+		p_emp_id, p_pay_month, v_basic, v_hra, v_special, v_other,
+        v_total_earnings, v_total_deductions, v_net_pay, 'Finalized', NOW(), 'Auto-generated'
+    );
+   END //
+   
+   DELIMITER ;
